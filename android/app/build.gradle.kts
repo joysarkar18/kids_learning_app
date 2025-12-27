232466ab625124c06335ef012127fe910de5d337
joy sarkar <joysarkar8171@gmail.com>
2025-12-27 18:05:11 +0530

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.byteberg.kids_learning"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
